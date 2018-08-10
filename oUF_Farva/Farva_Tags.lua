6a1df18f27d22a100dc8fffda3ab4bd5ab975ad4
Corey Russ <cruss.seq11@gmail.com>
2018-08-10 12:01:16 -0700

    Clean up unused tags

--- oUF_Farva/Farva_Tags.lua
+++ oUF_Farva/Farva_Tags.lua
@@ -85,17 +85,9 @@ oUF.Tags.Methods['player:hp']  = function(u)
     elseif not UnitIsConnected(u) then
         return '|cff559655 D/C|r'
     elseif (min<max) then
-       -- if (power > 0) then
-          --  return ('|cffAF5050'..sValue(min))..' | '..math.floor(min/max*100+.5)..'%'..('|cff559655 || ')..hex(r, g, b)..sValue(power)
-        --else
             return ('|cffAF5050'..sValue(min))..' | '..math.floor(min/max*100+.5)..'%'
-        --end
     else
-       -- if (power > 0) then
-         --   return ('|cff559655'..sValue(min))..('|cff559655 || ')..hex(r, g, b)..sValue(power)
-        --else
             return ('|cff559655'..sValue(min))
-		--end
     end
 end
 oUF.Tags.Events['player:hp'] = 'UNIT_HEALTH UNIT_POWER_UPDATE UNIT_CONNECTION'
@@ -103,10 +95,7 @@ oUF.Tags.Events['player:hp'] = 'UNIT_HEALTH UNIT_POWER_UPDATE UNIT_CONNECTION'
 
 oUF.Tags.Methods['color'] = function(u, r)
     local reaction = UnitReaction(u, 'player')
-    --if (UnitIsTapped(u) and not UnitIsTappedByPlayer(u)) then
-      --  return hex(oUF.colors.tapped)
-    --else
-	if (UnitIsPlayer(u)) and not cfg.class_colorbars then
+		if (UnitIsPlayer(u)) and not cfg.class_colorbars then
         local _, class = UnitClass(u)
         return hex(oUF.colors.class[class])
     elseif reaction and not (UnitIsPlayer(u)) then
@@ -116,7 +105,6 @@ oUF.Tags.Methods['color'] = function(u, r)
     end
 end
 oUF.Tags.Events['color'] = 'UNIT_HEALTH'
-
 
 oUF.Tags.Methods["afkdnd"] = function(unit)
 	if unit then
@@ -162,66 +150,6 @@ oUF.Tags.Methods['raid:hp'] = function(u)
 end
 oUF.Tags.Events['raid:hp'] = 'UNIT_HEALTH UNIT_CONNECTION'
 
---oUF.Tags.Methods["raidhpname"] = function(u)
---local name = UnitName(u)
---local c, m = UnitHealth(u), UnitHealthMax(u)
---if c == 0 then return "RIP" elseif(UnitIsGhost(u)) then return "GHO" elseif(not UnitIsConnected(u)) then return "OFF" elseif c < m then return oUF.Tags.Methods["missinghp"](u) else return utf8sub(name, 6, false) end end
---oUF.Tags.Events["raidhpname"] = "UNIT_HEALTH UNIT_MAXHEALTH UNIT_NAME_UPDATE UNIT_CONNECTION"
-
-oUF.Tags.Methods["druidPower"] = function(unit)
-	local min, max = UnitPower(unit, 0), UnitPowerMax(unit, 0)
-	return UnitPowerType(unit) ~= 0 and format('|cff0ff0ff %d|r', min)
-end
-oUF.Tags.Events["druidPower"] = 'UNIT_MAXPOWER UNIT_POWER_UPDATE UPDATE_SHAPESHIFT_FORM'
-
-oUF.Tags.Methods["holyPower"] = function(unit)
-local hp = UnitPower('player', SPELL_POWER_HOLY_POWER)
-	if hp > 0 then
-		return string.format("|CFFfcf514%d|r",hp)
-	end
-end
-oUF.Tags.Events["holyPower"] = "UNIT_POWER_UPDATE"
-
-oUF.Tags.Methods["soulShards"] = function(unit)
-local sn = UnitPower('player', SPELL_POWER_SOUL_SHARDS)
-	if sn == 3 then
-		return	-- only show shard number for less than 3 shards
-	else
-		return string.format("|CFF911fff%d|r",sn)
-	end
-end
-oUF.Tags.Events["soulShards"] = "UNIT_POWER_UPDATE"
-
-local EVANGELISM = GetSpellInfo(81661) or GetSpellInfo(81660)
-local DARK_EVANGELISM = GetSpellInfo(87118) or GetSpellInfo(87117)
-oUF.Tags.Methods["evangelism"] = function(unit)
-	if unit == "player" then
-		local name, _, icon, count = UnitBuff("player", EVANGELISM)
-		if name then return count end
-		--name, _, icon, count = UnitBuff("player", DARK_EVANGELISM)
-		return name and count
-	end
-end
-oUF.Tags.Events["evangelism"] = "UNIT_AURA"
-
-local SHADOW_ORB = GetSpellInfo(77487)
-oUF.Tags.Methods["shadoworbs"] = function(unit)
-	if unit == "player" then
-		local name, _, icon, count = UnitBuff("player", SHADOW_ORB)
-		return name and count
-	end
-end
-oUF.Tags.Events["shadoworbs"] = "UNIT_AURA"
-
-local MAELSTROM_WEAPON = GetSpellInfo(53817)
-oUF.Tags.Methods["maelstrom"] = function(unit)
-	if unit == "player" then
-		local name, _, icon, count = UnitBuff("player", MAELSTROM_WEAPON)
-		return name and count
-	end
-end
-oUF.Tags.Events["maelstrom"] = "UNIT_AURA"
-
 oUF.Tags.Methods['altpower'] = function(u)
 	local cur = UnitPower(u, ALTERNATE_POWER_INDEX)
 	local max = UnitPowerMax(u, ALTERNATE_POWER_INDEX)
@@ -236,71 +164,5 @@ oUF.Tags.Methods['altpower'] = function(u)
 	end
 end
 oUF.Tags.Events['altpower'] = 'UNIT_POWER_UPDATE UNIT_MAXPOWER'
-
----------------
--- raid tags --
----------------
-
--- priest
-oUF.Tags.Methods["NivPWS"] = function(u)
-	if UnitAura(u, GetSpellInfo(17) or "Power Word: Shield") then return "|cffFFD800S|r" end end
-oUF.Tags.Methods["NivRenew"] = function(u)
-	local name, _,_,_,_,_,_, fromwho,_ = UnitAura(u, GetSpellInfo(139) or "Renew")
-	if not (fromwho == "player") then return end
-	if UnitAura(u, GetSpellInfo(139) or "Renew") then return "|cff00FF10R|r" end end
-oUF.Tags.Methods["NivPoM"] = function(u) local c = select(4, UnitAura(u, GetSpellInfo(33076) or "Prayer of Mending")) if c then return "|cffFFCF7FPoM("..c..")|r" end end
-oUF.Tags.Methods["NivGS"] = function(u) if UnitAura(u, GetSpellInfo(47788) or "Guardian Spirit") then return "|cffFFD800G|r" end end
-oUF.Tags.Methods["NivWS"] = function(u) if UnitDebuff(u, GetSpellInfo(6788) or "Weakened Soul") then return "|cffFF0000 W|r" end end
-oUF.Tags.Methods["NivFW"] = function(u) if UnitAura(u, GetSpellInfo(6346) or "Fear Ward") then return "|cffFF0000 FW|r" end end
-
-oUF.Tags.Methods["NivPWS"] = "UNIT_AURA"
-oUF.Tags.Methods["NivPWB"] = "UNIT_AURA"
-oUF.Tags.Methods["NivRenew"] = "UNIT_AURA"
-oUF.Tags.Methods["NivPoM"] = "UNIT_AURA"
-oUF.Tags.Methods["NivGS"] = "UNIT_AURA"
-oUF.Tags.Methods["NivWS"] = "UNIT_AURA"
-oUF.Tags.Methods["NivFW"] = "UNIT_AURA"
-
--- paladin
-oUF.Tags.Methods["NivBoL"] = function(u) if UnitAura(u, GetSpellInfo(53563) or "Beacon of Light") then return "|cffFFD800BoL|r" end end
-
-oUF.Tags.Methods["NivBoL"] = "UNIT_AURA"
-
--- druid
-oUF.Tags.Methods["NivRej"] = function(u)
-	local name, _,_,_,_,_,_, fromwho,_ = UnitAura(u, GetSpellInfo(774) or "Rejuvenation")
-	if not (fromwho == "player") then return end
-	if UnitAura(u, GetSpellInfo(774) or "Rejuvenation") then return "|cff00FEBFRj|r" end end
-oUF.Tags.Methods["NivReg"] = function(u)
-	local name, _,_,_,_,_,_, fromwho,_ = UnitAura(u, GetSpellInfo(8936) or "Regrowth")
-	if not (fromwho == "player") then return end
-	if UnitAura(u, GetSpellInfo(8936) or "Regrowth") then return "|cff00FF10Rg|r" end end
-oUF.Tags.Methods["NivLB"] = function(u)
-	local name, _,_,_,_,_,_, fromwho,_ = UnitAura(u, GetSpellInfo(33763) or "Lifebloom")
-	if not (fromwho == "player") then return end
-	local c = select(4, UnitAura(u, GetSpellInfo(33763) or "Lifebloom")) if c then return "|cffA7FD0ALB("..c..")|r" end end
-oUF.Tags.Methods["NivInn"] = function(u) if UnitAura(u, GetSpellInfo(29166) or "Innervate") then return "|cff3c3be7Iv|r" end end
-oUF.Tags.Methods["NivWG"] = function(u) if UnitAura(u, GetSpellInfo(53251) or "Wild Growth") then return "|cff33FF33WG|r" end end
-
-oUF.Tags.Methods["NivRej"] = "UNIT_AURA"
-oUF.Tags.Methods["NivReg"] = "UNIT_AURA"
-oUF.Tags.Methods["NivLB"] = "UNIT_AURA"
-oUF.Tags.Methods["NivInn"] = "UNIT_AURA"
-oUF.Tags.Methods["NivWG"] = "UNIT_AURA"
-
--- shaman
-oUF.Tags.Methods["NivES"] = function(u) local c = select(4, UnitAura(u, GetSpellInfo(974) or "Earth Shield")) if c then return "|cffFFCF7FES("..c..")|r" end end
-oUF.Tags.Methods["NivRipT"] = function(u)
-	local name, _,_,_,_,_,_, fromwho,_ = UnitAura(u, GetSpellInfo(61295) or "Riptide")
-	if not (fromwho == "player") then return end
-	if UnitAura(u, GetSpellInfo(61295) or "Riptide") then return "|cff00FEBFR|r" end end
-oUF.Tags.Methods["NivErLiv"] = function(u)
-	local name, _,_,_,_,_,_, fromwho,_ = UnitAura(u, GetSpellInfo(52000) or "Earthliving")
-	if not (fromwho == "player") then return end
-	if UnitAura(u, GetSpellInfo(51945) or "Earthliving") then return "|cff00FEBFErEL|r" end end
-
-oUF.Tags.Methods["NivES"] = "UNIT_AURA"
-oUF.Tags.Methods["NivRipT"] = "UNIT_AURA"
-oUF.Tags.Methods["NivErLiv"] = "UNIT_AURA"
 
 ns.tags = tags
