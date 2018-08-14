7fb48e70e3a0a190ffe746ea3eacec27d025f3ff
Corey Russ <cruss.seq11@gmail.com>
2018-08-13 21:05:40 -0700

    fix taint with protected function

--- oUF_Farva/Farva.lua
+++ oUF_Farva/Farva.lua
@@ -1569,19 +1569,6 @@ if cfg.disableRaidFrameManager then
         CompactRaidFrameContainer:Hide()
 end
 
--- remove SET_FOCUS & CLEAR_FOCUS from menu, to prevent errors
-do
-    for k,v in pairs(UnitPopupMenus) do
-        for x,y in pairs(UnitPopupMenus[k]) do
-            if y == "SET_FOCUS" then
-                table.remove(UnitPopupMenus[k],x)
-            elseif y == "CLEAR_FOCUS" then
-                table.remove(UnitPopupMenus[k],x)
-            end
-        end
-    end
-end
-
 -------------
 -- The End --
 -------------
